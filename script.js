adb9a122c00a678a847728f53d8d2323ba3b930c
tahereh <tahereh@gamil.com>
2023-12-13 07:40:47 -0800

    challenge shop js

---
+++ script.js
@@ -0,0 +1,61 @@
+// Array
+let shop = [
+
+    // Array
+    products = [
+
+        // Arrays
+        laptop = [
+
+            // Objects
+            {
+                name : 'ASUS',
+
+            },
+
+            {
+                name : 'Lenovo'
+            },
+
+            {
+                name : 'MacBook'
+            }
+
+        ],
+
+        phone = [
+
+            {
+                name : 'Samsung',
+
+            },
+
+            {
+                name : 'Apple'
+            },
+
+            {
+                name : 'Xiaomi'
+            }
+
+        ],
+
+        watch = [
+
+            {
+                name : 'Breitling',
+
+            },
+
+            {
+                name : 'Rolex'
+            },
+
+            {
+                name : 'Omega'
+            }
+
+        ],
+
+    ]
+]
